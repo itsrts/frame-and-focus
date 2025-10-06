40b5a6249b7077be4ccc85eb601fb3d73514a0f2
shubham mishra <shubhammishra3070@gmail.com>
2025-10-06 15:38:42 +0000

    Increase the speed of marquee a little bit.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -97,7 +97,7 @@ const config = {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'marquee': 'marquee 40s linear infinite',
+        'marquee': 'marquee 25s linear infinite',
       },
     },
   },
