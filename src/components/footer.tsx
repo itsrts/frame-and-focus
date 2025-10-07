f6ae93ade7e06caddc274bc6196c7e82f08ba6c6
shubham mishra <shubhammishra3070@gmail.com>
2025-10-07 17:53:41 +0000

    Reduce the height of footer, make it fit the content with padding of 30p

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
 
   return (
     <footer className="bg-background border-t">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-6 md:py-8">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="flex flex-col md:flex-row justify-between items-center gap-4">
           <div className="flex items-center">
             <Logo className="w-48" />
